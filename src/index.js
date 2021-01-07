0b5f0397f516ce2a23232c605610f53a11c842f2
Henry Liang <HL55127@worksafebc.com>
2021-01-07 09:13:31 -0800

    Add ie11 fixes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import 'react-app-polyfill/ie11';
+import 'core-js/stable'
+// import 'react-app-polyfill/stable';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
